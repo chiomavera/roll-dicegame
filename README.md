11c6d3b3bb7c263a9c013c835de5317a56026705
chiomavera <you@example.com>
2022-05-03 06:29:54 +0100

    readme updated, html, css and js files added.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# roll-dicegame
+# roll-diceGame
+this is a web game dice between two users. 
+whenever the browser is refreshed the program runs ans outputs who is the winner of that round.
---
+++ index.js
@@ -0,0 +1,30 @@
+// if (windows.performance.navigation.type === 1){
+//     rollDice();
+// }
+
+let randomNumber1 = Math.floor(6 * Math.random()) + 1; //select from pseudo whole No from 1-6.
+let randomNumber2 = Math.floor(6 * Math.random()) + 1;
+
+let randomImageSource1 = "images/dice" + randomNumber1 + ".png" ;/*using the random No variable to select 
+                                                                a dice image randomly from the source (dice1-dice6)*/
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+
+let image1 = document.querySelectorAll("img")[0]; //select the first image from html document
+let image2 = document.querySelectorAll("img")[1]; //select the second image from html document
+
+image1.setAttribute("src", randomImageSource1);
+image2.setAttribute("src", randomImageSource2);
+
+let message;
+if (randomNumber1 > randomNumber2) {//if player 1 wins
+    message = "🚩 Player 1 Wins";
+}
+else if (randomNumber1 < randomNumber2) {//if player 2 wins
+    message = "🚩 player 2 Wins";
+}
+else{//if there is a draw
+    message = "draw!";
+}
+
+document.querySelector("h1").innerHTML = message;
